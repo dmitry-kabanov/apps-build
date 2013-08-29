df96482c6280ca91da3d96e23ea4bca43fb2a137
Dmitry Kabanov <kabanovdmitry@gmail.com>
2013-08-30 00:47:51 +0300

    Add script that creates symlinks in $HOME to config and function files.

---
+++ create-links.sh
@@ -0,0 +1,4 @@
+cd $HOME
+ln -s $DEV/app-build/script/apps.conf .apps.conf
+ln -s $DEV/app-build/script/apps-compiler.sh .apps-compiler.sh
+ln -s $DEV/app-build/script/apps-functions.sh .apps-functions.sh
