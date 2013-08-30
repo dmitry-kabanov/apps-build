561e5eeeca4ce062c72f803213daa979c211e9f1
Dmitry Kabanov <kabanovdmitry@gmail.com>
2013-08-30 20:13:45 +0300

    Add loading of module of particular compiler before building.

--- apps-compiler.sh
+++ apps-compiler.sh
@@ -5,7 +5,12 @@ fi
 
 comp_fam_ver=$1
 
+. $APPS_MODULEFILES_INSTALL_DIR/../lmod/lmod/init/profile
+module purge
+
 if [ $comp_fam_ver == intel-2013u4 ] ; then
     comp_fam=intel
     comp_ver=2013u4
 fi
+
+module load $comp_fam/$comp_ver
